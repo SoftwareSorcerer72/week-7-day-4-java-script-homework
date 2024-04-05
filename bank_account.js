acad3dfe4ea08e3b458fb6e08247cee421a4a482
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-05 04:58:57 -0400

    Created account class.

---
+++ bank_account.js
@@ -0,0 +1,15 @@
+// Need to create an Account class with the properties of accountNumber, current balance, and owner.
+// The account should have medtods to withdraw and deposit.
+// The withdraw method needs to check if there is enough money to withdraw before withdrawing.
+
+// Implement a child class CheckingAccount and SavingsAccount, each inheriting from the Account class.
+
+// The CheckingAccount will also have an overdraftLimit property. Override the withdraw method to first check if there is enough (+ overdraftLimit) before withdrawing.
+
+// The SavingsAccount will also have an interestRate. Add a method addInterest that will increase the currentBalance by that interest rate.
+
+class Account (accountNumber, currentBalance, owner) {
+this.accountNumber = accountNumber;
+this.currentBalance = currentBalance;
+this.owner = owner;
+}
